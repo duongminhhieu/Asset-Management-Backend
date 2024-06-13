5ed3b57bc0c0796dcd1c7a3e4fb10732fc5cd120
Hoang Nguyen <hoangnd.work@gmail.com>
2024-06-13 11:34:56 +0700

    Init

---
+++ src/main/java/com/nashtech/rookie/asset_management_0701/AssetManagement0701Application.java
@@ -0,0 +1,13 @@
+package com.nashtech.rookie.asset_management_0701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetManagement0701Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetManagement0701Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nashtech/rookie/asset_management_0701/AssetManagement0701ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nashtech.rookie.asset_management_0701;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetManagement0701ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
